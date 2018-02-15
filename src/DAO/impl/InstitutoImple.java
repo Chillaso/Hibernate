11aef13e04fb49ed07d05467252e336eb8ea9f3a
Chillaso <carlete2@hotmail.com>
2018-02-15 23:21:25 +0100

    Implementacion de las consultas de profesor e instituto Signed-off-by:Chillaso <carlete2@hotmail.com>

--- src/DAO/impl/InstitutoImple.java
+++ src/DAO/impl/InstitutoImple.java
@@ -3,45 +3,89 @@ package DAO.impl;
 //@author chillaso
 
 import DAO.InstitutoDAO;
+import Modelo.Asignatura;
 import Modelo.Instituto;
+import Util.HibernateUtil;
 import java.util.Collection;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 
 public class InstitutoImple implements InstitutoDAO{
 
     @Override
-    public Collection<Instituto> getAll() {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Collection<Instituto> getAll() 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	Criteria c = s.createCriteria(Instituto.class);
+	Collection<Instituto> list = (Collection<Instituto>) c.list();
+	s.close();
+	return list;
     }
 
     @Override
-    public Instituto getInstituto(int id_instituto) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instituto getInstituto(int id_instituto) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	Criteria c = s.createCriteria(Instituto.class);
+	c.add(Restrictions.eq("id_instituto", id_instituto));
+	Instituto i = (Instituto) c.list().get(0);
+	s.close();
+	return i;	
     }
 
     @Override
-    public Instituto getInstituto(String nombre) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instituto getInstituto(String nombre) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	Criteria c = s.createCriteria(Instituto.class);
+	c.add(Restrictions.eq("nombre", nombre));
+	Instituto i = (Instituto) c.list().get(0);
+	s.close();
+	return i;	
     }
 
     @Override
-    public void insert(Instituto a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void insert(Instituto a) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.save(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
     @Override
-    public void delete(Instituto a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(Instituto a) 
+    {
+		Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.delete(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
     @Override
     public void update(Instituto a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.update(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
     @Override
-    public void saveOrUpdate(Instituto a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void saveOrUpdate(Instituto a) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.saveOrUpdate(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
 }
--- src/DAO/impl/ProfesorImple.java
+++ src/DAO/impl/ProfesorImple.java
@@ -1,46 +1,97 @@
 package DAO.impl;
 
 import DAO.ProfesorDAO;
+import Modelo.Alumno;
+import Modelo.Instituto;
 import Modelo.Profesor;
+import Util.HibernateUtil;
 import java.util.Collection;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 /**@author Chillaso*/
 
 public class ProfesorImple implements ProfesorDAO{
 
     @Override
-    public Collection<Profesor> getAll() {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Collection<Profesor> getAll() 
+    {		
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	Criteria c = s.createCriteria(Profesor.class);
+	Collection<Profesor> list = (Collection<Profesor>) c.list();
+	s.close();
+	return list;
     }
 
     @Override
-    public Profesor getProfesor(int id_profesor) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Profesor getProfesor(int id_profesor) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	Criteria c = s.createCriteria(Profesor.class);
+	c.add(Restrictions.eq("id_profesor", id_profesor));
+	Profesor i = (Profesor) c.list().get(0);
+	s.close();
+	return i;	
     }
 
     @Override
-    public Profesor getProfesor(String nombre, boolean dni) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Profesor getProfesor(String nombre, boolean dni) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	Criteria c = s.createCriteria(Profesor.class);
+	
+	if(dni)
+	    c.add(Restrictions.eq("dni", nombre));
+	else 
+	    c.add(Restrictions.eq("nombre", nombre));
+	
+	Profesor p = (Profesor) c.list().get(0);
+	s.close();
+	return p;	
     }
 
     @Override
-    public void insert(Profesor a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void insert(Profesor a) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.save(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
     @Override
-    public void delete(Profesor a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(Profesor a) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.delete(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
     @Override
     public void update(Profesor a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.update(a);
+	s.getTransaction().commit();
+	s.close();
     }
 
     @Override
-    public void saveOrUpdate(Profesor a) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void saveOrUpdate(Profesor a) 
+    {
+	Session s = HibernateUtil.getSessionFactory().openSession();
+	s.beginTransaction();
+	s.saveOrUpdate(a);
+	s.getTransaction().commit();
+	s.close();
     }
+
 
 }
